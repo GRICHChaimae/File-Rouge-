825832e86be42176a2e2ae53c2ec033b82eac401
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2022-05-09 00:12:16 +0000

    first

---
+++ ReadingTime/View/views/Home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Style/Home.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <dvi class="menu">
+            <div class="logo">
+                <a href="Home.html"><h3>Reading</h3><img src="../Images/logobrowny.png" alt="ReadingTime"></a>
+            </div>
+            <div class="centerMenu">
+                <ul>
+                    <li><a href="Home.php">Home</a></li>
+                    <li><a href="Blog.php">Blog</a></li>
+                    <li><a href="WhyUs.php">Why Us</a></li>
+                </ul>
+            </div>
+            <div class="rightMenu">
+                <ul>
+                    <li><a href="LogIn.php">login</a></li>
+                    <li>/</li>
+                    <li><a href="SignUp.php">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
